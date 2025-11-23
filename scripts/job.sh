112e61a9ec2558bca44ff0774cd0388ef1b95dc3
Milad Rasouli <milad.rasouli@tuta.io>
2025-11-23 12:50:34 +0330

    ci: update

--- scripts/job.sh
+++ scripts/job.sh
@@ -6,6 +6,16 @@
 countries=(
   "Berlin"
   "Germany"
+  "Netherlands"
+  "Finland"
+  "Sweden"
+  "Denmark"
+  "Norway"
+  "Estonia"
+  "Ireland"
+  "United Kingdom"
+  "Portugal"
+  "Spain"
 )
 
 # Proxy list
